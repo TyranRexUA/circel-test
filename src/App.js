932e5163e9d5a4014d38e9cda6aa16931f8a4bfb
TyranRexUA <tyranrexua@gmail.com>
2021-04-02 17:49:06 +0300

    7

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn tReact
+          Learn React
         </a>
       </header>
     </div>
